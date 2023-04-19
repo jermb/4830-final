c88a307f66d456ecd4c989b7df75fec632b9af9c
jermb <77866654+jermb@users.noreply.github.com>
2023-04-19 15:03:59 -0500

    created component

--- BookNook/src/app/app.module.ts
+++ BookNook/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { SearchComponent } from './search/search.component';
+import { BookListComponent } from './book-list/book-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    SearchComponent,
+    BookListComponent
   ],
   imports: [
     BrowserModule,
