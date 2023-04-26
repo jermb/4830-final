6fcb15e2c0f7d87d487c8d0f9b4924cf6776eea4
jermb <77866654+jermb@users.noreply.github.com>
2023-04-26 16:34:09 -0500

    User service update

--- BookNook/src/app/book-list/book-list.component.ts
+++ BookNook/src/app/book-list/book-list.component.ts
@@ -1,32 +1,62 @@
-import { Component } from '@angular/core';
-import { UserService } from '../user/user.service';
-import { Book } from '../book.model';
+// import { Component } from '@angular/core';
+// import { UserService } from '../user/user.service';
+// import { Book } from '../book.model';
 
-@Component({
-  selector: 'app-book-list',
-  templateUrl: './book-list.component.html',
-  styleUrls: ['./book-list.component.css']
-})
-export class BookListComponent {
+// @Component({
+//   selector: 'app-book-list',
+//   templateUrl: './book-list.component.html',
+//   styleUrls: ['./book-list.component.css']
+// })
+// export class BookListComponent {
 
-  private user: UserService;
-  bookmarks: Book[];
-  favorites: {book: Book, score?: number}[];
+//   private user: UserService;
+//   bookmarks: Book[];
+//   favorites: {book: Book, score?: number}[];
 
-  constructor(user: UserService) {
-    this.user = user;
+//   constructor(user: UserService) {
+//     this.user = user;
 
-  }
+//   }
 
-  // private setFavorites()
+//   // private setFavorites()
 
-  private setBookmarks(books: string[]) {
+//   private setBookmarks(books: string[]) {
 
-  }
+//   }
 
 
-  score(id: string, val: number) {
+//   score(id: string, val: number) {
 
-  }
+//   }
 
-}
+// }
+
+
+
+// import { Component, OnInit, OnDestroy } from '@angular/core';
+// import { Subscription } from 'rxjs';
+// import { UserService } from '../user/user.service';
+// import { Book } from '../book.model';
+
+
+// @Component({
+//   selector: 'app-book-list',
+//   templateUrl: './book-list.component.html',
+//   styleUrls: ['./book-list.component.css']
+// })
+// export class BookListComponent implements OnInit, OnDestroy{
+//     bookmarks: Book[];
+//   favorites: {book: Book, score?: number}[];
+//   private bookSub: Subscription;
+
+//   constructor(public userService: UserService){}
+//   ngOnInit() {
+//     this.userService.getMarkedBooks();
+//     this.bookSub = this.userService.getBookUpdateListener().subscribe((books: Book[])=>{
+//       this.books = books;
+//     })
+//   }
+//   ngOnDestroy() {
+//     this.bookSub.unsubscribe();
+//   }
+// }
