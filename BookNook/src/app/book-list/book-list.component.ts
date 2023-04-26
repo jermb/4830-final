4b5c8329cea9cb6990bce6076f8a8c154c20c81b
cdswpv <cdswpv@umsystem.edu>
2023-04-26 15:52:43 -0500

    Updated book-list component

--- BookNook/src/app/book-list/book-list.component.ts
+++ BookNook/src/app/book-list/book-list.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UserService } from '../user/user.service'; 
+
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit, OnDestroy{
+  books: User[] = []
+  private bookSub: Subscription;
 
+  constructor(public userService: UserService){}
+  ngOnInit() {
+    this.userService.getMarkedBooks();
+    this.bookSub = this.userService.getBookUpdateListener().subscribe((books: BookList[])=>{
+      this.books = books;
+    })
+  }
+  ngOnDestroy() {
+    this.bookSub.unsubscribe();
+  }
 }
